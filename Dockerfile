e585080c9957f5be422060dac8770325302ab194
Juliano Xavier <julianotutti03@gmail.com>
2022-01-21 03:59:18 -0300

    files

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,21 @@
-FROM python:3.7
+
+FROM python:3.8.3-alpine
+
+WORKDIR /usr/frexcodesafio
+
+
+ENV PYTHONDONTWRITEBYTECODE 1
 ENV PYTHONUNBUFFERED 1
-RUN mkdir /frexcodesafio
-WORKDIR /frexcodesafio  
-ADD requirements.txt /frexcodesafio/
-RUN  pip install -r requirements.txt
-ADD . /frexcodesafio/
+
+RUN apk update
+RUN pip install --upgrade pip
+RUN apk add python3-dev
+RUN apk add gpgme-dev
+RUN apk add libc-dev
+RUN apk add gcc jpeg-dev zlib-dev libffi-dev freetype-dev musl-dev lcms2-dev openjpeg-dev tiff-dev tk-dev tcl-dev cairo-dev pango-dev gdk-pixbuf-dev
+RUN apk add mariadb-dev
+COPY ./requirements.txt .
+RUN pip install -r requirements.txt
+
+# copy project
+COPY . .
--- frexcodesafio/settings.py
+++ frexcodesafio/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-v%^*qz@!n4y=%oiua+#gu^(olpls@6om2cshar-dvp5h@^v9nk'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = int(os.environ.get("DEBUG", default=0))
 
-ALLOWED_HOSTS = ['127.0.0.1','0.0.0.0']
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
 
 
 # Application definition
@@ -79,13 +80,13 @@ WSGI_APPLICATION = 'frexcodesafio.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'frexco_api',
-        'USER': 'admin',
-        'PASSWORD': 'slipknot00',
-        'HOST':'db',
-        'PORT':'3306',
-        'OPTIONS':{
-            'init_command':"SET sql_mode='STRICT_TRANS_TABLES'"},
+        'NAME': 'api',
+        'USER': 'root',
+        'PASSWORD': 'apassword',
+        'HOST': 'db',
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"},
     }
 }
 
